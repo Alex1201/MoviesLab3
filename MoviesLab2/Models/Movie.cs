ac0a59116ea35e8479df7e1e0739b963822601c3
AlexM <alexandru.muresan12@gmail.com>
2020-05-22 21:44:24 +0300

    first commit

---
+++ MoviesLab2/Models/Movie.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MoviesLab2.Models
+{
+    public class Movie
+    {
+        public long Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public Gender Gender { get; set; }
+        public float Duration { get; set; }
+        public string YearOfRelease { get; set; }
+        public string Director { get; set; }
+        public DateTime DateAdded { get; set; }
+        public int Rating { get; set; }
+        public bool Watched { get; set; }
+    }
+}
